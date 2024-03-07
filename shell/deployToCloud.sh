b76a951d024c49bedb6138f0dbab7b445be6c3a4
wlongabaugh <wlongabaugh@systemsbiology.org>
2024-03-06 18:18:14 -0800

    More built-in SA cleanup

--- shell/deployToCloud.sh
+++ shell/deployToCloud.sh
@@ -3,13 +3,12 @@
 #
 # Script for deployment from the desktop
 
-trap 'rm ./privatekey.json; mv hold_config.txt config.txt' EXIT
+trap 'mv hold_config.txt config.txt' EXIT
 
 source ./setEnvVars.sh
 cd ..
 mv config.txt hold_config.txt
 gsutil cp "${PROXY_CONFIG_GCS_PATH}" ./config.txt
-#gsutil cp "${PROXY_SA_KEY_GCS_PATH}" ./privatekey.json
 
 GOT_IT=`grep ${PROXY_SERVICE_NAME} app.yaml`
 
