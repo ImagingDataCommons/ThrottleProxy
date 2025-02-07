3f8d721f70b59953b26c454a18337be1dbfc3335
wlongabaugh <wlongabaugh@systemsbiology.org>
2025-02-07 15:42:58 -0800

    Enhancements for easy desktop setup

--- shell/prepareBackend.sh
+++ shell/prepareBackend.sh
@@ -10,6 +10,7 @@ REDIS_SIZE_GB=1
 VPC_MIN_INSTANCES=2
 VPC_MAX_INSTANCES=3
 VPC_MACHINE_TYPE="e2-micro"
+USING_AE_STANDARD="TRUE or FALSE, using AppEngine Standard"
 
 # NOTE! Hardwired variables below to configure REDIS and the VPC connector! Edit if you don't like them
 
@@ -45,7 +46,8 @@ gcloud redis instances create ${REDIS_NAME} --size=${REDIS_SIZE_GB} --region=${R
 # use AppEngine Flex, this is not needed. Change min, max instances ond machine type as desirec
 #
 
-gcloud compute networks vpc-access connectors create ${VAC_NAME} \
+if [ "${USING_AE_STANDARD}" = "TRUE" ]; then
+  gcloud compute networks vpc-access connectors create ${VAC_NAME} \
        --region ${REGION} \
        --network default \
        --range 10.1.0.0/28 \
@@ -53,6 +55,7 @@ gcloud compute networks vpc-access connectors create ${VAC_NAME} \
        --max-instances ${VPC_MAX_INSTANCES} \
        --machine-type ${VPC_MACHINE_TYPE} \
        --project=${PROJECT_ID}
+fi
 
 
 #
